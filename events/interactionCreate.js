ea119d37420fb35e9b544f410ef05296e2dee9dc
Allison Poppe <allisonpoppe89@gmail.com>
2021-08-30 13:05:47 -0500

    Update console logging when a command is used to be tied to devMode

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,10 @@
+const Config = require("../config.json");
+
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+		if (Config.devMode) {
+			console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+		}
 	}
 };
