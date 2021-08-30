89ed2ec0068a14e1e88132d0fe7fc25402a4bded
Allison Poppe <allisonpoppe89@gmail.com>
2021-08-30 17:03:00 -0500

    Finish event listeners for client

---
+++ events/voice-state-update.js
@@ -0,0 +1,10 @@
+const Config = require("../config.json");
+
+module.exports = {
+	name: "voiceStateUpdate",
+	execute(oldMember, newMember) {
+        if (Config.devMode) {
+		    console.log("Voice State Update!");
+        }
+	}
+};
