bcc1130956b559cb590aa52b27006e5394507f77
Allison Poppe <allisonpoppe89@gmail.com>
2021-08-30 19:48:01 -0500

    Fix now playing embed going over text limits bug

--- commands/now-playing.js
+++ commands/now-playing.js
@@ -7,28 +7,25 @@ module.exports = {
     async execute(interaction) {
         await interaction.deferReply();
         if (QueueManager.getInstance().queues.has(interaction.guildId)) {
-            if (QueueManager.getInstance().getNotificationIsOn(interaction.guildId)) {
-                let currentSong = QueueManager.getInstance().queues.get(interaction.guildId).currentlyPlayingSong;
-                const embed = new Discord.MessageEmbed()
-                    .setColor("#0099FF")
-                    .setAuthor("NOW PLAYING - A Cappella", "https://allisonpoppe.com/acappella/images/logo.png")
-                    .setThumbnail("https://allisonpoppe.com/acappella/images/logo.png")
-                    .setTimestamp()
-                    .setFooter("by Allison Poppe", "https://allisonpoppe.com/acappella/images/logo.png");
+            let currentSong = QueueManager.getInstance().queues.get(interaction.guildId).currentlyPlayingSong;
+            const embed = new Discord.MessageEmbed()
+                .setColor("#0099FF")
+                .setAuthor("NOW PLAYING - A Cappella", "https://allisonpoppe.com/acappella/images/logo.png")
+                .setThumbnail("https://allisonpoppe.com/acappella/images/logo.png")
+                .setTimestamp()
+                .setFooter("by Allison Poppe", "https://allisonpoppe.com/acappella/images/logo.png");
 
-                if (currentSong) {
-                    let currentLength = new Date(currentSong.length * 1000).toISOString().substr(11, 8);
-                    embed.addField("\u200B", "\u200B");
-                    embed.addField("CURRENTLY PLAYING", "[" + currentSong.title + "](" + currentSong.url + ")");
-                    embed.addField("Requested by " + currentSong.requesterName,  "Length: " + currentLength.toString());
-                    embed.addField("Description", currentSong.description);
-                } else {
-                    embed.addField("\u200B", "\u200B");
-                    embed.addField("NOTHING CURRENTLY PLAYING", "");
-                    embed.addField("\u200B", "\u200B");
-                }
-                return await interaction.followUp({embeds: [embed]});
+            if (currentSong) {
+                let currentLength = new Date(currentSong.length * 1000).toISOString().substr(11, 8);
+                embed.addField("\u200B", "\u200B");
+                embed.addField("CURRENTLY PLAYING", "[" + currentSong.title + "](" + currentSong.url + ")");
+                embed.addField("Requested by " + currentSong.requesterName,  "Length: " + currentLength.toString());
+            } else {
+                embed.addField("\u200B", "\u200B");
+                embed.addField("NOTHING CURRENTLY PLAYING", "");
+                embed.addField("\u200B", "\u200B");
             }
+            return await interaction.followUp({embeds: [embed]});
         } else {
             return await interaction.followUp({content: "There's nothing currently playing on this server"});
         }
